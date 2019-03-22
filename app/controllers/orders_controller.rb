63be1ae1cd77cd0b862120b2a602c32df092e782
John <connollyjohnf@gmail.com>
2019-03-22 00:45:06 +0000

    two emails are sent on order confirmation, one html, one plain text. Confirmation of sending is seen in console.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,9 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      Notifier.order_confirm_email(current_user, order).deliver
       redirect_to order, notice: 'Your Order has been placed.'
+
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,2 +1,10 @@
 class Notifier < ApplicationMailer
+    default from: 'no-reply@jungle.com'
+
+    def order_confirm_email(user, order)
+        @user = user
+        @order = order
+        mail(to: @user.email, subject: "Order ##{order.id} confirmation with Jungle")
+    end
+
 end
